e0ccf9405924157385fecc486484ecba3e547b20
alexmsalmeida <aa2369@login-p-2.data.cluster>
2023-08-23 13:41:31 +0100

    Update preprocess

--- code/preproc.R
+++ code/preproc.R
@@ -12,7 +12,7 @@ cat("Reading input file...\n")
 data_raw <- read.csv(args[2], row.names = 1, check.names = FALSE)
 
 cat("Preprocessing data...\n")
-data_processed <- preprocess_data(data_raw, outcome_colname = args[3], remove_var = "zv")
+data_processed <- preprocess_data(data_raw, outcome_colname = args[3], remove_var = "zv", filter_threshold = (nrow(data_raw)-1) * 0.01)
 
 cat("Saving final dataframe...\n")
 saveRDS(data_processed, file = args[4])
